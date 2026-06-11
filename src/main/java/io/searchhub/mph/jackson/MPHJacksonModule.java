c4bc057117e45f9cf9848e67762f0794421153b7
Rudolf Batt <rudolf.batt@commerce-experts.com>
2026-06-11 16:23:29 +0200

    Remove unused code

--- src/main/java/io/searchhub/mph/jackson/MPHJacksonModule.java
+++ src/main/java/io/searchhub/mph/jackson/MPHJacksonModule.java
@@ -1,15 +1,11 @@
 package io.searchhub.mph.jackson;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
 import com.fasterxml.jackson.databind.type.MapType;
 import io.searchhub.mph.MPHStringMap;
 import io.searchhub.mph.PackageVersion;
-
-import java.util.List;
 
 public class MPHJacksonModule extends SimpleModule {
 
@@ -18,16 +14,8 @@ public class MPHJacksonModule extends SimpleModule {
 		super.addSerializer(MPHStringMap.class, new MPHStringMapSerializer());
 		super.addDeserializer(MPHStringMap.class, new MPHStringMapDeserializer());
 
-		super.setMixInAnnotation(MPHStringMap.SerializableData.class, SerializableDataAnnotations.class);
-		super.setSerializerModifier(new BeanSerializerModifier() {
-
-			@Override
-			public JsonSerializer<?> modifyMapSerializer(SerializationConfig config, MapType valueType, BeanDescription beanDesc, JsonSerializer<?> mapSerializer) {
-				// Return a delegating serializer for all but the MPHStringMapSerializer
-				return mapSerializer instanceof MPHStringMapSerializer ? mapSerializer : new DelegatingMapSerializer(mapSerializer);
-			}
-		});
 		super.setDeserializerModifier(new BeanDeserializerModifier() {
+
 			@Override
 			public JsonDeserializer<?> modifyMapDeserializer(DeserializationConfig config, MapType type, BeanDescription beanDesc, JsonDeserializer<?> deserializer) {
 				return new DelegatingMapDeserializer(deserializer);
@@ -35,9 +23,4 @@ public class MPHJacksonModule extends SimpleModule {
 		});
 	}
 
-	public static abstract class SerializableDataAnnotations<V> {
-
-		@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@type")
-		protected List<V> values;
-	}
 }
